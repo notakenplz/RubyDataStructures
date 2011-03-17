731f3e056bc1c00caa4a0a37b805aff40462b19f
daniel <daniel@daniel-virtual-machine.(none)>
2011-03-16 23:50:49 -0700

    first

---
+++ bst.rb
@@ -0,0 +1,23 @@
+class BinarySearchTree
+  # To change this template use File | Settings | File Templates.
+
+  bstArray = []
+
+  public
+  def insert(value)
+  end
+
+  def delete(value)
+  end
+
+  def contains(value)
+  end
+
+  def getLeftChild(value)
+
+  end
+
+  def getRightChild(value
+
+  end
+end
---
+++ bst_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+
+describe "Binary Search Tree" do
+
+  it "should Act like a BST" do
+
+  
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
